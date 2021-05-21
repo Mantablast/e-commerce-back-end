662f5bbf99c0985851a2852c21f1ac082c0b14f9
AimeeJay <ajesso@post.com>
2021-05-20 22:26:19 -0400

    added product tag and product basic model definitions

--- models/Product.js
+++ models/Product.js
@@ -4,20 +4,37 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
+//means product table
 Product.init(
   {
-  // trying id, prime key and have auto incriment, setting up like mysql constraints
-  //required: id, product_name, price, stock, category_id
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  production
+    // trying id, prime key and have auto incriment, setting up like mysql constraints
+    //required: id, product_name, price, stock, category_id
+    //allowNulL: false >> must be filled in
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: falst
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -3,10 +3,24 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
-
+// setting up like mysql constraints
+//product tag will require id, 
+//allowNulL: false >> must be filled in
+//exampe from seeds
+// {
+//   product_id: 1,
+//   tag_id: 6,
+// }
 ProductTag.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   },
   {
     sequelize,

30b16479e59038cb545ecdc0805ce15bb1ab1714
AimeeJay <ajesso@post.com>
2021-05-22 13:08:20 -0400

    category model was missing id

--- models/Category.js
+++ models/Category.js
@@ -11,6 +11,14 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+  }
+  },
+  {
     category_name: {
     type: DataTypes.STRING,
     allowNull: false

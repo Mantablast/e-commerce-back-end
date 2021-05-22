e45e22819a2397b8bc25ae39b3fe168ad45fda0a
AimeeJay <ajesso@post.com>
2021-05-21 22:09:05 -0400

    category routes complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { request } = require('express');
 const { Category, Product } = require('../../models');
 
 //*Used example from just-tech-news
@@ -21,26 +22,80 @@ Category.findAll(
     model: Product
   }
   }
-).then(categoryPost => {
-  res.json(categoryPost);
+).then(categoryGet => {
+  res.json(categoryGet);
 });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products  //probably use findOne
+  Category.findOne(
+  {
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product
+    }
+  }
+  ).then(categoryGet => {
+    res.json(categoryGet);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category  //Create
+  //category seed example
+  // {
+  //   category_name: 'Shirts',
+  // }
+  console.log(req.body);
+  //tell it how to create post
+  Category.create({
+    category_name: req.body.category_name
+  })
+  //send to json
+  .then(categoryPost => {
+    res.json(categoryPost);
+  });
 });
+  
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value //Update
+  //seed example
+  // {
+  //   "id": 6,
+  //   "category_name": "Underwear"
+  // }
+Category.update(
+  //instruct where
+  {
+    where: {
+      id: req.params.id,
+    },
+  },
+  {
+    category_name: req.body.category_name
+  }
+).then(categoryUpdate => {
+  res.json(categoryUpdate);
+});
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value  //Destroy
+  Category.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(categoryDestroy => {
+    res.json(categoryDestroy);
+  });
 });
+
 
 module.exports = router;
